44b81259ff89101b4627d11a4ff69a0161847463
wukai <wu.vkcyan@gmail.com>
2023-04-18 17:38:52 +0800

    去除无用引入

--- packages/src/components.d.ts
+++ packages/src/components.d.ts
@@ -1,5 +1,5 @@
-import * as components from './index'
 import { IButton } from './components/Button'
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     IButton: typeof IButton
